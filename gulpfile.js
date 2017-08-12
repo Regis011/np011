bd6a2887467e3968d3f09e7308d00ffb64666043
Regis011 <vladimir.rancic@np011.se>
2017-08-12 12:36:45 +0200

    update on gulp, bower, npm and booststrap(sass) files

--- gulpfile.js
+++ gulpfile.js
@@ -14,12 +14,17 @@ var gulp = require( 'gulp' ),
   sass = require( 'gulp-sass' ),
   imagemin = require('gulp-imagemin'),
   bower = require('gulp-bower'),
+  cleanCSS = require('gulp-clean-css'),
+  concatCss = require('gulp-concat-css'),
+  concat = require('gulp-concat'),
+  sourcemaps = require('gulp-sourcemaps');
+
 
 var config = {
      bowerDir: './bower_components' 
 }
- 
- 
+
+
 // Default error handler
 var onError = function( err ) {
   console.log( 'An error occured:', err.message );
@@ -36,23 +41,35 @@ gulp.task('icons', function() { 
     return gulp.src(config.bowerDir + '/fontawesome/fonts/**.*') 
         .pipe(gulp.dest('./fonts')); 
 });
- 
- 
+
+
 // Jshint outputs any kind of javascript problems you might have
 // Only checks javascript files inside /src directory
+/*
 gulp.task( 'jshint', function() {
   return gulp.src( './js/src/*.js' )
     .pipe( jshint() )
     .pipe( jshint.reporter( stylish ) )
     .pipe( jshint.reporter( 'fail' ) );
 })
- 
- 
+*/
+// Minify Custom JavaScript files
+gulp.task('custom-scripts', function() {
+  return gulp.src('./js/src/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(uglify())
+    .pipe(concat('custom.min.js'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./js/dist/'));
+});
+
+
 // Concatenates all files that it finds in the manifest
 // and creates two versions: normal and minified.
 // It's dependent on the jshint task to succeed.
-gulp.task( 'scripts', ['jshint'], function() {
+gulp.task( 'scripts', ['custom-scripts'], function() {
   return gulp.src( './js/manifest.js' )
+    .pipe(sourcemaps.init())
     .pipe( include() )
     .pipe( rename( { basename: 'scripts' } ) )
     .pipe( gulp.dest( './js/dist' ) )
@@ -60,11 +77,11 @@ gulp.task( 'scripts', ['jshint'], function() {
     // remove the following 3 lines if you don't want it
     .pipe( uglify() )
     .pipe( rename( { suffix: '.min' } ) )
+    .pipe(sourcemaps.write())
     .pipe( gulp.dest( './js/dist' ) )
     .pipe(notify({ message: 'scripts task complete' }))
-    .pipe( livereload() );
 } );
- 
+
 // As with javascripts this task creates two files, the regular and
 // the minified one. It automatically reloads browser as well.
 var options = {};
@@ -72,10 +89,11 @@ options.sass = {
   errLogToConsole: true,
   sourceMap: 'sass',
   sourceComments: 'map',
+  outputStyle: 'nested',
   precision: 10,
   //imagePath: 'assets/img',
   includePaths: [
-    config.bowerDir + '/bootstrap-sass/assets/stylesheets/',
+    config.bowerDir + '/bootstrap-sass/assets/stylesheets',
     config.bowerDir + '/fontawesome/scss',
   ]
 };
@@ -87,17 +105,31 @@ options.autoprefixer = {
 
 gulp.task('sass', function() {
   return gulp.src('./sass/style.scss')
+    .pipe(sourcemaps.init())
     .pipe( plumber( { errorHandler: onError } ) )
     .pipe(sass(options.sass))
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4',
       options.autoprefixer
       ))
-    .pipe( gulp.dest( '.' ) )
     .pipe( minifycss() )
     .pipe( rename( { suffix: '.min' } ) )
+    .pipe(sourcemaps.write())
     .pipe( gulp.dest( '.' ) )
     .pipe(notify({ message: 'sass task complete' }))
     .pipe( livereload() );
+});
+
+gulp.task('style', function() {
+  return gulp.src(['css/**/*.css', 'css/*.css'])
+    .pipe(concatCss("custom.css"))
+    .pipe( minifycss() )
+    .pipe( rename( {
+      prefix: "style.",
+      basename: 'custom',
+      suffix: '.min'
+    } ) )
+    .pipe(gulp.dest('.'))
+    .pipe(notify({ message: 'custom style complete' }));
 });
 
 // Optimize Images
@@ -107,26 +139,26 @@ gulp.task('images', function() {
     .pipe(gulp.dest('./images'))
     .pipe(notify({ message: 'Images task complete' }));
 });
- 
- 
+
+
 // Start the livereload server and watch files for change
-gulp.task( 'watch', function() {
+gulp.task( 'dev', function() {
   livereload.listen();
- 
+
   // don't listen to whole js folder, it'll create an infinite loop
   gulp.watch( [ './js/**/*.js', '!./js/dist/*.js' ], [ 'scripts' ] )
- 
+
   gulp.watch( './sass/**/*.scss', ['sass'] );
 
-  gulp.watch('./images/**/*', ['images']);
- 
+  gulp.watch( ['css/**/*.css', 'css/*.css'], ['style'] );
+
+  gulp.watch( './images/**/*', ['images']);
+
   gulp.watch( './**/*.php' ).on( 'change', function( file ) {
     // reload browser whenever any PHP file changes
     livereload.changed( file );
   } );
 } );
- 
- 
-gulp.task( 'default', ['bower', 'icons'], function() {
- // Does nothing in this task, just triggers the dependent 'watch'
-} );
+
+
+gulp.task( 'default', ['bower', 'icons', 'scripts','sass','style','images']);
