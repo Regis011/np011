06d11d2f94c7259c808ce3a2a9cac3e7de39c0f5
Vladimir Rancic <vladimir.rancic@np011.se>
2020-10-23 17:03:00 +0200

    udpated js script path for theme and upgrade packages

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -13,7 +13,7 @@ if ( ! function_exists( 'np011_scripts' ) ) {
 		// Get the theme data.
 		$the_theme = wp_get_theme();
 		wp_enqueue_style( 'np011-styles', get_stylesheet_directory_uri() . '/assets/css/style.css', false, null );
-		wp_enqueue_script( 'np011-scripts', get_template_directory_uri() . '/js/build.js', ['jquery'], null, true );
+		wp_enqueue_script( 'np011-scripts', get_template_directory_uri() . '/assets/js/build.js', ['jquery'], null, true );
 		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 			wp_enqueue_script( 'comment-reply' );
 		}
